93e115afc32ce44ead6fe6c209a5287c8f791ebd
potsables <postables@rtradetechnologies.com>
2019-02-10 00:26:00 -0800

    add number of users count

--- cmd/tfarmer/main.go
+++ cmd/tfarmer/main.go
@@ -7,11 +7,11 @@ import (
 	"os"
 
 	"github.com/RTradeLtd/gorm"
+	"github.com/RTradeLtd/tfarmer/user"
 
 	"github.com/RTradeLtd/cmd"
 	"github.com/RTradeLtd/config"
 	"github.com/RTradeLtd/database"
-	"github.com/RTradeLtd/database/models"
 )
 
 // Version denotes the tag of this build
@@ -77,43 +77,31 @@ func newDB(cfg config.TemporalConfig, noSSL bool) (*gorm.DB, error) {
 
 var commands = map[string]cmd.Cmd{
 	"user": {
-		Hidden:      true,
-		Blurb:       "create a user",
-		Description: "Create a Temporal user. Provide args as username, password, email. Do not use in production.",
-		Args:        []string{"user", "pass", "email"},
-		Action: func(cfg config.TemporalConfig, args map[string]string) {
-			fmt.Printf("creating user '%s' (%s)...\n", args["user"], args["email"])
-			d, err := database.Initialize(&cfg, database.Options{
-				SSLModeDisable: *dbNoSSL,
-				RunMigrations:  *dbMigrate,
-			})
-			if err != nil {
-				fmt.Println("failed to initialize database connection", err)
-				os.Exit(1)
-			}
-			// create user account
-			if _, err := models.NewUserManager(d.DB).NewUserAccount(
-				args["user"], args["pass"], args["email"],
-			); err != nil {
-				fmt.Println("failed to create user account", err)
-				os.Exit(1)
-			}
-			// add credits
-			if _, err := models.NewUserManager(d.DB).AddCredits(args["user"], 99999999); err != nil {
-				fmt.Println("failed to grant credits to user account", err)
-				os.Exit(1)
-			}
-			// generate email activation token
-			userModel, err := models.NewUserManager(d.DB).GenerateEmailVerificationToken(args["user"])
-			if err != nil {
-				fmt.Println("failed to generate email verification token", err)
-				os.Exit(1)
-			}
-			// activate email
-			if _, err := models.NewUserManager(d.DB).ValidateEmailVerificationToken(args["user"], userModel.EmailVerificationToken); err != nil {
-				fmt.Println("failed to activate email", err)
-				os.Exit(1)
-			}
+		Blurb:         "User based metrics",
+		Description:   "Allows for gathering of user based metric information (usage, activity, etc..)",
+		ChildRequired: true,
+		Children: map[string]cmd.Cmd{
+			"registered": {
+				Blurb:       "Registered users",
+				Description: "Used to get the number of registered users",
+				Action: func(cfg config.TemporalConfig, args map[string]string) {
+					db, err := database.Initialize(&cfg, database.Options{
+						SSLModeDisable: *dbNoSSL,
+						RunMigrations:  *dbMigrate,
+					})
+					if err != nil {
+						fmt.Println("failed to initialize database connection", err.Error())
+						os.Exit(1)
+					}
+					uf := user.NewFarmer(db.DB)
+					users, err := uf.RegisteredUsers()
+					if err != nil {
+						fmt.Println("failed to get registered users", err.Error())
+					}
+					numberOfUsers := len(users)
+					fmt.Printf("there are %v total registered users", numberOfUsers)
+				},
+			},
 		},
 	},
 }
